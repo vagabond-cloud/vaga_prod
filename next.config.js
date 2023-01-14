4974f59bcb39390498f9e1efd4fbf646fd1eaf9c
TRXF <hello@trxf.net>
2023-01-14 14:15:56 +0100

    build folder

--- next.config.js
+++ next.config.js
@@ -2,6 +2,7 @@ module.exports = {
   images: {
     domains: [''],
   },
+  distDir: 'build',
   images: {
     domains: ['ipfs.infura.io', 'firebasestorage.googleapis.com', 'ipfs.infura.io', 'countryflagsapi.com'],
   },
